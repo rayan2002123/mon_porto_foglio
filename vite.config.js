84b2ccb88c348d417dc7c1c71111321a92b63634
rayan2002123 <rngasseu123@gmail.com>
2026-04-29 01:49:41 +0200

    fix build for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    host: true,
-    allowedHosts: "all",
-    hmr: {
-      protocol: "wss",
-      host: "eaved-suzi-shaftless.ngrok-free.dev"
-    }
-  }
+  plugins: [react()],
+  base: "./"
 });
